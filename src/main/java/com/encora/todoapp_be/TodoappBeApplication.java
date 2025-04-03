48c075f543997a2d8d618bba41f439cd4bdbe567
Brian A <brian.acosta@encora.com>
2025-04-03 12:24:11 -0600

    Initial commit

---
+++ src/main/java/com/encora/todoapp_be/TodoappBeApplication.java
@@ -0,0 +1,13 @@
+package com.encora.todoapp_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encora/todoapp_be/TodoappBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.todoapp_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
