d385fb70e2093d401118ac03ba7d095e59c89bad
Brian A <brian.acosta@encora.com>
2025-04-09 23:21:58 -0600

    add crossOrigin link to front-end

--- src/main/java/com/encora/todoapp_be/controller/TodoController.java
+++ src/main/java/com/encora/todoapp_be/controller/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.encora.todoapp_be.service.TodoService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:8080")
 public class TodoController {
 
     private final TodoService todoService;
